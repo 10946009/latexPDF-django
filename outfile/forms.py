251183fdc57f9b434c42844abfbb911862753050
品 <10946009@ntub.edu.tw>
2024-02-18 16:36:52 +0800

    修正表單寫法&排版

--- outfile/forms.py
+++ outfile/forms.py
@@ -1,40 +1,48 @@
 from django import forms
-from django.forms import ModelForm ,inlineformset_factory
+from django.forms import ModelForm, inlineformset_factory
 from outfile.models import Problem, InputOutput
 from django.forms import formset_factory
+
+
 class ProblemForm(ModelForm):
     class Meta:
         model = Problem
-        fields = '__all__'
+        fields = "__all__"
         labels = {
-            'title': '題目名稱',
-            'timelimit': '時間限制',
-            'statement': '題目敘述',
-            'input_format': '範例輸入',
-            'output_format': '範例輸出',
-            'hint': 'hint',
+            "title": "題目名稱",
+            "timelimit": "時間限制",
+            "statement": "題目敘述",
+            "input_format": "範例輸入",
+            "output_format": "範例輸出",
+            "hint": "hint",
         }
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'timelimit': forms.NumberInput(attrs={'class': 'form-control'}),
-            'statement': forms.Textarea(attrs={'class': 'form-control'}),
-            'input_format': forms.Textarea(attrs={'class': 'form-control'}),
-            'output_format': forms.Textarea(attrs={'class': 'form-control'}),
-            'hint': forms.Textarea(attrs={'class': 'form-control'}),
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "timelimit": forms.NumberInput(attrs={"class": "form-control"}),
+            "statement": forms.Textarea(attrs={"class": "form-control"}),
+            "input_format": forms.Textarea(attrs={"class": "form-control"}),
+            "output_format": forms.Textarea(attrs={"class": "form-control"}),
+            "hint": forms.Textarea(attrs={"class": "form-control"}),
         }
 
 class InputOutputForm(ModelForm):
+
     class Meta:
         model = InputOutput
-        fields ="input","output"
-        exclude = ['id']  # 排除掉 id 字段
+        fields = ["input", "output" , "is_sample"]
+        exclude = ["id"]  # 排除掉 id 字段
         labels = {
-            'input': '輸入',
-            'output': '輸出',
+            "input": "輸入",
+            "output": "輸出",
+            "is_sample": "範例",
         }
         widgets = {
-            'input': forms.Textarea(attrs={'class': 'form-control'}),
-            'output': forms.Textarea(attrs={'class': 'form-control'}),
+            "input": forms.Textarea(attrs={"class": "form-control"}),
+            "output": forms.Textarea(attrs={"class": "form-control"}),
+            "is_sample": forms.CheckboxInput(attrs={"class": "form-check-input"}),
         }
 
-InputOutputFormSet = inlineformset_factory(Problem, InputOutput, form=InputOutputForm,extra=0, can_delete=True)
+
+InputOutputFormSet = inlineformset_factory(
+    Problem, InputOutput, form=InputOutputForm, extra=0, can_delete=True
+)
