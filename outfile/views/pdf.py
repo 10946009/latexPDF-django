3130cccc1cddf6c1ab97975a3774524e1fc4b686
品 <10946009@ntub.edu.tw>
2024-02-26 21:11:42 +0800

    避免PDF使用舊資料-2

--- outfile/views/pdf.py
+++ outfile/views/pdf.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from outfile.models import Problem
 from .PathManager import PathManager
+import time
 
 def get(request,cid):
     path_manager = PathManager(cid)
     show_pdf = path_manager.exist_problem_pdf()
-    return render(request, 'create_form_PDF.html',{'cid':cid,'show_pdf':show_pdf})
+    timestamp = int(time.time())
+    return render(request, 'create_form_PDF.html', {'cid':cid, 'show_pdf':show_pdf, 'timestamp': timestamp})
